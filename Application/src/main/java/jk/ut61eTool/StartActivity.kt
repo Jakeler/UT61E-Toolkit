7f78deb329d8355b8e2df29fd69bf365b9b7dd50
Jake <jakel@protonmail.com>
2017-11-21 00:22:25 +0100

    Buttons on Startscreen

--- Application/src/main/java/jk/ut61eTool/StartActivity.kt
+++ Application/src/main/java/jk/ut61eTool/StartActivity.kt
@@ -1,7 +1,10 @@
 package jk.ut61eTool
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class StartActivity : Activity() {
 
@@ -10,6 +13,11 @@ class StartActivity : Activity() {
         setContentView(R.layout.activity_start)
     }
 
-    fun startScan() {
+    fun startScan(v : View) {
+        startActivity(Intent(this, DeviceScanActivity::class.java))
+    }
+
+    fun startView(v: View) {
+        Toast.makeText(this,"Not implemented, stay tuned for updates!", Toast.LENGTH_SHORT).show();
     }
 }

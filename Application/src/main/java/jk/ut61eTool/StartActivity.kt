2a6e9a3d8095cb52804bd86f197afb32d937ac5e
Jake <jakel@protonmail.com>
2017-11-20 23:23:37 +0100

    Add new Launcher Activity

---
+++ Application/src/main/java/jk/ut61eTool/StartActivity.kt
@@ -0,0 +1,15 @@
+package jk.ut61eTool
+
+import android.app.Activity
+import android.os.Bundle
+
+class StartActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+
+    fun startScan() {
+    }
+}

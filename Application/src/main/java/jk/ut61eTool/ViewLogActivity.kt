53f5e512bd9ede689399975ac8b835f3080062a8
Jake <git@ja-ke.tech>
2018-05-10 22:52:16 +0200

    Add CSV parsing and display to graph

--- Application/src/main/java/jk/ut61eTool/ViewLogActivity.kt
+++ Application/src/main/java/jk/ut61eTool/ViewLogActivity.kt
@@ -3,7 +3,12 @@ package jk.ut61eTool
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import com.github.mikephil.charting.charts.BarChart
+import com.jake.UT61e_decoder
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import java.io.File
+
 
 class ViewLogActivity : Activity() {
 
@@ -11,11 +16,25 @@ class ViewLogActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_log)
 
-        val file = intent.extras["filename"]
+        var graphUI = GraphUI(this, findViewById<BarChart>(R.id.view_graph), findViewById(R.id.view_dataInfo))
+        graphUI.viewSize = Int.MAX_VALUE
 
-        if (file is File) {
-            Log.d("FILE", file.toString())
-            Log.w("PATH", file.path)
+        val file = intent.extras["filename"] as File
+
+        Log.d("FILE", file.length().toString())
+
+        var csvFormat = CSVFormat.RFC4180.withDelimiter(';').withCommentMarker('#')
+        val parser = CSVParser.parse(file, charset("UTF-8"), csvFormat)
+        parser.recordNumber
+        for (csvRecord in parser) {
+            if (csvRecord.hasComment()) continue
+            var data = UT61e_decoder()
+            data.value = csvRecord[0].toDoubleOrNull()?: continue
+            data.unit_str = csvRecord[1]
+            graphUI.displayData(data)
         }
+
+
+
     }
 }

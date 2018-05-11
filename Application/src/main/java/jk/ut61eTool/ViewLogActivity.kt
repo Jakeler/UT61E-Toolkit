f61ade49cfd8a74450ab3fc40f6f6b122765c194
Jake <git@ja-ke.tech>
2018-05-11 18:22:48 +0200

    Make some local vars immutable

--- Application/src/main/java/jk/ut61eTool/ViewLogActivity.kt
+++ Application/src/main/java/jk/ut61eTool/ViewLogActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import com.github.mikephil.charting.charts.BarChart
 import com.jake.UT61e_decoder
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
@@ -21,7 +20,7 @@ class ViewLogActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_log)
 
-        var graphUI = GraphUI(this, findViewById<BarChart>(R.id.view_graph), findViewById(R.id.view_dataInfo), R.color.logPrimary)
+        val graphUI = GraphUI(this, findViewById(R.id.view_graph), findViewById(R.id.view_dataInfo), R.color.logPrimary)
         graphUI.viewSize = Int.MAX_VALUE
 
         val file = intent.extras["filename"] as File
@@ -30,7 +29,7 @@ class ViewLogActivity : Activity() {
 
         actionBar.title = getString(R.string.logview_actionBar_title, file.name)
 
-        var csvFormat = CSVFormat.RFC4180.withDelimiter(';').withCommentMarker('#')
+        val csvFormat = CSVFormat.RFC4180.withDelimiter(';').withCommentMarker('#')
         val parser = CSVParser.parse(file, charset("UTF-8"), csvFormat)
 
         val record_list = parser.records
@@ -42,6 +41,7 @@ class ViewLogActivity : Activity() {
         }
 
 
+
         val spinner = findViewById<Spinner>(R.id.log_spinner)
         spinner.adapter = ArrayAdapter<String>(this, R.layout.spinneritem_time, R.id.time_textView, time_strings)
 
@@ -49,11 +49,11 @@ class ViewLogActivity : Activity() {
             graphUI.graph.barData.removeDataSet(0)
             graphUI.newDataSet()
 
-            var start_pos = time_list[spinner.selectedItemPosition].recordNumber.toInt()
+            val start_pos = time_list[spinner.selectedItemPosition].recordNumber.toInt()
             for (pos in start_pos until record_list.size) {
                 val csvRecord = record_list[pos]
                 if (csvRecord.hasComment()) break
-                var data = UT61e_decoder()
+                val data = UT61e_decoder()
                 data.value = csvRecord[0].toDoubleOrNull()?: continue
                 data.unit_str = csvRecord[1]
                 graphUI.displayData(data)

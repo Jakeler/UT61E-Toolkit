67da6ac079d8635ea9a43690ab99eab35cb525f1
Jake <git@ja-ke.tech>
2018-05-12 11:27:47 +0200

    Use fragment in settings activity

--- Application/src/main/java/jk/ut61eTool/SettingsActivity.java
+++ Application/src/main/java/jk/ut61eTool/SettingsActivity.java
@@ -1,27 +1,32 @@
 package jk.ut61eTool;
 
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.util.Log;
-import android.view.View;
+import android.preference.PreferenceFragment;
 
 import java.util.Date;
 
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
+public class SettingsActivity extends PreferenceActivity{
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
         @Override
-        public void onCreate(Bundle savedInstanceState) {
+        public void onCreate(final Bundle savedInstanceState)
+        {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.prefs);
-            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
             Preference preference = findPreference("info");
             preference.setSummary("Version: " + BuildConfig.VERSION_NAME + "\n" + new Date(BuildConfig.TIMESTAMP));
         }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
     }
 }

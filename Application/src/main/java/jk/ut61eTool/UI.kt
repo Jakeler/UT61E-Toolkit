4a6106a372d6a7c6ea913d8803d98f7626a2cd1a
Jake <git@ja-ke.tech>
2021-02-18 14:01:52 +0100

    Clean up UI code with binding and kotlin
    refs #12

--- Application/src/main/java/jk/ut61eTool/UI.kt
+++ Application/src/main/java/jk/ut61eTool/UI.kt
@@ -1,52 +1,40 @@
 package jk.ut61eTool
 
-import android.app.Activity
 import android.view.View
-import android.widget.TextView
 import com.jake.UT61e_decoder
+import jk.ut61eTool.databinding.LogActivityBinding
 
-class UI(a: Activity) {
-    var mDataField: TextView
-    var neg: TextView
-    var ol: TextView
-    var acdc: TextView
-    var freqDuty: TextView
-
+class UI(val binding: LogActivityBinding) {
 
     fun update(ut61e: UT61e_decoder) {
-        mDataField.text = ut61e.toString()
-        enableTextView(neg, ut61e.getValue() < 0)
-        enableTextView(ol, ut61e.isOL)
+        binding.dataValue.text = ut61e.toString()
+        enableTextView(binding.Neg, ut61e.getValue() < 0)
+        enableTextView(binding.OL, ut61e.isOL)
+
         if (ut61e.isFreq || ut61e.isDuty) {
-            enableTextView(freqDuty, true)
-            enableTextView(acdc, false)
-            if (ut61e.isDuty) freqDuty.text = "Duty" else if (ut61e.isFreq) freqDuty.text = "Freq."
+            enableTextView(binding.FreqDuty, true)
+            enableTextView(binding.ACDC, false)
+            binding.FreqDuty.text = when {
+                ut61e.isDuty -> "Duty"
+                ut61e.isFreq -> "Freq."
+                else -> ""
+            }
         } else {
-            enableTextView(freqDuty, false)
-            enableTextView(acdc, true)
-            if (ut61e.isDC) {
-                acdc.text = "DC"
-            } else if (ut61e.isAC) {
-                acdc.text = "AC"
-            } else {
-                enableTextView(acdc, false)
+            enableTextView(binding.FreqDuty, false)
+            enableTextView(binding.ACDC, true)
+
+            binding.ACDC.text = when {
+                ut61e.isDC -> "DC"
+                ut61e.isAC -> "AC"
+                else -> {
+                    enableTextView(binding.ACDC, false)
+                    ""
+                }
             }
         }
     }
 
     private fun enableTextView(v: View, enabled: Boolean) {
-        if (enabled) {
-            v.alpha = 1.0f
-        } else {
-            v.alpha = 0.2f
-        }
-    }
-
-    init {
-        mDataField = a.findViewById(R.id.data_value)
-        neg = a.findViewById(R.id.Neg)
-        ol = a.findViewById(R.id.OL)
-        acdc = a.findViewById(R.id.ACDC)
-        freqDuty = a.findViewById(R.id.FreqDuty)
+        v.alpha = if (enabled) 1.0f else 0.2f
     }
 }

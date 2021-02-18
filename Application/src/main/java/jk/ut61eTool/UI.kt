895f99ec97d0b226b4ae52be684d4e09a4e43378
Jake <git@ja-ke.tech>
2021-02-18 13:41:17 +0100

    Convert UI model to kotlin

--- Application/src/main/java/jk/ut61eTool/UI.kt
+++ Application/src/main/java/jk/ut61eTool/UI.kt
@@ -1,52 +1,52 @@
-package jk.ut61eTool;
+package jk.ut61eTool
 
-import android.app.Activity;
-import android.view.View;
-import android.widget.TextView;
+import android.app.Activity
+import android.view.View
+import android.widget.TextView
+import com.jake.UT61e_decoder
 
-import com.jake.UT61e_decoder;
+class UI(a: Activity) {
+    var mDataField: TextView
+    var neg: TextView
+    var ol: TextView
+    var acdc: TextView
+    var freqDuty: TextView
 
-public class UI {
 
-    TextView mDataField;
-    TextView neg, ol, acdc, freqDuty;
-
-    public UI(Activity a) {
-        mDataField = a.findViewById(R.id.data_value);
-        neg = a.findViewById(R.id.Neg);
-        ol = a.findViewById(R.id.OL);
-        acdc = a.findViewById(R.id.ACDC);
-        freqDuty = a.findViewById(R.id.FreqDuty);
-    }
-
-    public void update(UT61e_decoder ut61e) {
-        mDataField.setText(ut61e.toString());
-
-        enableTextView(neg, ut61e.getValue() < 0);
-        enableTextView(ol, ut61e.isOL());
-        if (ut61e.isFreq() || ut61e.isDuty()) {
-            enableTextView(freqDuty, true);
-            enableTextView(acdc, false);
-            if (ut61e.isDuty()) freqDuty.setText("Duty");
-            else if (ut61e.isFreq()) freqDuty.setText("Freq.");
+    fun update(ut61e: UT61e_decoder) {
+        mDataField.text = ut61e.toString()
+        enableTextView(neg, ut61e.getValue() < 0)
+        enableTextView(ol, ut61e.isOL)
+        if (ut61e.isFreq || ut61e.isDuty) {
+            enableTextView(freqDuty, true)
+            enableTextView(acdc, false)
+            if (ut61e.isDuty) freqDuty.text = "Duty" else if (ut61e.isFreq) freqDuty.text = "Freq."
         } else {
-            enableTextView(freqDuty, false);
-            enableTextView(acdc, true);
-            if (ut61e.isDC()) {
-                acdc.setText("DC");
-            } else if (ut61e.isAC()) {
-                acdc.setText("AC");
+            enableTextView(freqDuty, false)
+            enableTextView(acdc, true)
+            if (ut61e.isDC) {
+                acdc.text = "DC"
+            } else if (ut61e.isAC) {
+                acdc.text = "AC"
             } else {
-                enableTextView(acdc, false);
+                enableTextView(acdc, false)
             }
         }
     }
 
-    private void enableTextView(View v, boolean enabled) {
+    private fun enableTextView(v: View, enabled: Boolean) {
         if (enabled) {
-            v.setAlpha(1.0f);
+            v.alpha = 1.0f
         } else {
-            v.setAlpha(0.2f);
+            v.alpha = 0.2f
         }
+    }
+
+    init {
+        mDataField = a.findViewById(R.id.data_value)
+        neg = a.findViewById(R.id.Neg)
+        ol = a.findViewById(R.id.OL)
+        acdc = a.findViewById(R.id.ACDC)
+        freqDuty = a.findViewById(R.id.FreqDuty)
     }
 }

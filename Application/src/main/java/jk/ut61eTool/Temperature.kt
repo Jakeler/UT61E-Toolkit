20c6566e60e9aa3b78b3ad138886865ad71978f8
Jake <git@ja-ke.tech>
2021-03-06 20:11:05 +0100

    Move temp reader parts into reusable functions

--- Application/src/main/java/jk/ut61eTool/Temperature.kt
+++ Application/src/main/java/jk/ut61eTool/Temperature.kt
@@ -8,39 +8,43 @@ data class Temperature(
         val value: Double,
 )
 
-fun getSensorCount(): Int {
-    var text: String
-    Runtime.getRuntime().exec(arrayOf("sh", "-c", "echo /sys/class/thermal/thermal_zone* | wc -w")).let { process ->
+fun runShellForOutput(cmd: Array<String>): List<String> {
+    var lines: List<String>
+    Runtime.getRuntime().exec(cmd).let { process ->
         process.waitFor()
-        text = InputStreamReader(process.inputStream).use { it.readText() }
+        lines = InputStreamReader(process.inputStream).use { it.readLines() }
         process.destroy()
     }
-    return text.trim().toInt()
+    return lines
+}
+
+fun getSensorCount(): Int {
+    val cmd = arrayOf("sh", "-c", "echo /sys/class/thermal/thermal_zone* | wc -w")
+    runShellForOutput(cmd).let {
+        return it[0].trim().toInt()
+    }
 }
 
 fun getAllTemps(count: Int): List<Temperature> {
     val dirs = (0 until count).joinToString(separator = " "){ i ->
         "/sys/class/thermal/thermal_zone$i"
     }
-    var lines: List<String>
     val cmd = arrayOf("sh", "-c", "for dir in $dirs; do echo $(cat \$dir/type) $(cat \$dir/temp); done ")
-    Runtime.getRuntime().exec(cmd).let { process ->
-        process.waitFor()
-        lines = InputStreamReader(process.inputStream).use { it.readLines() }
-        process.destroy()
+    runShellForOutput(cmd).let {
+        return it.mapIndexed(::lineToTemp)
     }
-    val temps = lines.mapIndexed {index: Int, s: String ->
-        val tokens = s.split(" ")
-        val name = tokens[0]
-        val value = tokens[1].toDouble()
-        Temperature(index, name, when {
-            (value > -40 && value < 85) -> value // Industrial temp range, already valid temp
-            (value > -1000 && value < 1000) -> value/10 // range 100-1000
-            (value > -10_000 && value < 10_000) -> value/100 // range 1_000-10_000
-            (value > -100_000 && value < 100_000) -> value/1000 // range 10_000-100_000
-            (value > -1_000_000 && value < 1_000_000) -> value/10000 // range 100_000-1_000_000
-            else -> Double.NaN
-        })
-    }
-    return temps
+}
+
+fun lineToTemp(index: Int, s: String): Temperature {
+    val tokens = s.split(" ")
+    val name = tokens[0]
+    val value = tokens[1].toDouble()
+    return Temperature(index, name, when {
+        (value > -40 && value < 85) -> value // Industrial temp range, already valid temp
+        (value > -1000 && value < 1000) -> value/10 // range 100-1000
+        (value > -10_000 && value < 10_000) -> value/100 // range 1_000-10_000
+        (value > -100_000 && value < 100_000) -> value/1000 // range 10_000-100_000
+        (value > -1_000_000 && value < 1_000_000) -> value/10000 // range 100_000-1_000_000
+        else -> Double.NaN
+    })
 }
